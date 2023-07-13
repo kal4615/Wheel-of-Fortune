12eeb29928e80c3cee9d54149a2437e01b7cb85e
Kevin Luong <kal4615@rit.edu>
2023-07-12 20:26:09 -0400

    commit django project

---
+++ myproject/home/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def home_page(request):
+  template = loader.get_template('home.html')
+  return HttpResponse(template.render())
