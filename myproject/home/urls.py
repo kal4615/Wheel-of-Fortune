737e2597ffad51bf92b48909f67bc3b66f418cb5
Kevin Luong <kal4615@rit.edu>
2023-07-13 14:53:01 -0400

    add play and home pages

--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home_page, name='home'),
+    path('', views.home_page, name='home'),
+    path('play/', views.play_page, name='play'),
 ]
--- myproject/home/views.py
+++ myproject/home/views.py
@@ -4,3 +4,7 @@ from django.template import loader
 def home_page(request):
   template = loader.get_template('home.html')
   return HttpResponse(template.render())
+
+def play_page(request):
+  template = loader.get_template('play.html')
+  return HttpResponse(template.render())
